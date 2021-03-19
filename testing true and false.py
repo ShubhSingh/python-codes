c7c0a51951bc5444f3f9f6bb2bcab864911c2cfd
NitinSingh14 <78851049+NitinSingh14@users.noreply.github.com>
2021-03-19 21:15:55 +0530

    Add files via upload

---
+++ testing true and false.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[34]:
+
+
+def test():
+    print
+
+test()
+
+class Testing:
+    def test():
+        print("hello")
+
+Testing.test()
+
+a = bool(input("enter the value: "))
+if (a==False):
+    print("Please enter some input")
+print(a)
+
+
+# scope of variables
+
+
+
+# In[ ]:
+
+
+
+
