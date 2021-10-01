5d08861ccac90f6d32bb3e89e29f67e223662556
Shubham <send2shubham@gmail.com>
2021-10-01 12:40:44 +0530

    Create Solution929.py

---
+++ Solution929.py
@@ -0,0 +1,9 @@
+# python3
+class Solution929:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        seen = set()
+        for email in emails:
+            name, domain = email.split('@') 
+            local = name.split('+')[0].replace('.', '')
+            seen.add(local + '@' + domain)
+        return len(seen)
